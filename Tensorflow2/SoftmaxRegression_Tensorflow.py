ddc4601c6a581ce732d02734336f5b594ff074ef
CSH <danielc95@naver.com>
2023-08-18 00:32:45 +0900

    SoftmaxRegression_Tensorflow done.

--- Tensorflow2/SoftmaxRegression_Tensorflow.py
+++ Tensorflow2/SoftmaxRegression_Tensorflow.py
@@ -10,7 +10,7 @@ model = tf.keras.models.Sequential([
 ])
 
 # Loss function은 categorical CE, optimizer는 SGD, LR은 0.001
-sgd = tf.keras.optimizers.SGD(learning_rate=0.001)
+sgd = tf.keras.optimizers.SGD(learning_rate=0.1)
 model.compile(optimizer=sgd,
               loss='categorical_crossentropy',
               )
@@ -24,6 +24,24 @@ y = zoo[:-10, -1:]
 
 print(zoo.shape) # (101, 17), 마지막 column은 0~6 중 어느 분류인지 표시 되어있다
 X = tf.constant(x)
-Y = tf.constant(y)
+Y = tf.constant(y, dtype=tf.int32)
+Y = tf.squeeze(tf.one_hot(Y, 7))
 
-model.fit(X, Y)
+model.fit(X, Y, epochs=500)
+
+print(tf.argmax(model(tf.constant(zoo[-10:, :-1])), 1))
+print(zoo[-10:, -1:])
+
+# Inference한 값
+# tf.Tensor([2 3 0 0 1 0 5 0 6 1], shape=(10,), dtype=int64)
+# 실제 답
+# [[2.]
+#  [3.]
+#  [0.]
+#  [0.]
+#  [1.]
+#  [0.]
+#  [5.]
+#  [0.]
+#  [6.]
+#  [1.]]
